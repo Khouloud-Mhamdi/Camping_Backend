8b826dad7f43140f6884150746c263acb5e41606
alaa-belhassen <ezserie123@gmail.com>
2024-06-04 18:28:06 +0200

    fix import

--- src/main/java/com/example/spring/camping/models/boutique/Product.java
+++ src/main/java/com/example/spring/camping/models/boutique/Product.java
@@ -1,5 +1,6 @@
 package com.example.spring.camping.models.boutique;
 
+import com.example.spring.camping.models.CampLocations.Photo;
 import com.example.spring.camping.models.ManageUsers.Alimentation;
 import com.example.spring.camping.models.TypeProduct;
 import com.fasterxml.jackson.annotation.JsonIgnore;
